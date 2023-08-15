27f28010a260efd7746158e9bb8f6207c1162e3d
davgrigore <davgrigore@deloittece.com>
2023-08-15 17:14:31 +0300

    arrays exercises

--- arrays.js
+++ arrays.js
@@ -158,3 +158,129 @@ const total = number.reduce((accumulator, currentValue) => {
 }, 0);
 
 console.log(total);
+
+// Array from Range
+
+const nnumbers = arrayFromRange (-10, -4);
+
+console.log(nnumbers);
+
+function arrayFromRange(min, max) {
+    const outoput = [];
+    for (let i = min; i <= max; i++)
+    outoput.push(i);
+    return outoput;
+}
+
+//Includes
+
+const secondNumbers = [1, 2, 3, 4];
+
+console.log(secondNumbers.includes(1));
+
+function includes (otherNumbers, searchElement) {
+    for (let element of array)
+        if (element === searchElement)
+            return true;
+    return false;
+}
+
+//Except
+
+const thirdNumbers = [1, 2, 3, 4, 1, 1];
+
+const output = except (thirdNumbers, [1, 2]);
+
+console.log(output);
+
+function except (array, excluded) {
+    const output = [];
+    for (let element of array)
+        if (!excluded.includes(element))
+            output.push(element);
+    return output;
+}
+
+//Moving an Element
+
+const forthNumbers = [1, 2, 3, 4];
+
+const forthOutput = move(forthNumbers, 0, 1);
+
+console.log(forthOutput);
+
+function move (array, index, offset) {
+    const position = index + offset;
+    if (position >= array.length || position < 0) {
+        console.error('Invalid offset.');
+        return;
+    }
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(index + offset, 0, element);
+    return output;
+}
+
+//Count Occurences
+
+const fifthNumbers = [1, 2, 3, 4, 1];
+
+const count = countOccurences(fifthNumbers, 1);
+
+console.log(count);
+
+function countOccurences(array, searchElement) {
+    // let count = 0;
+    // for (let element of array)
+    //     if (element === searchElement)
+    //         count++;
+    // return count;
+
+    return array.reduce((accumulator, current) => {
+        const occurence = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurence;
+    }, 0)
+}
+
+//Get Max
+
+const sixthNumbers = [1, 2, 3, 4];
+
+const max = getMax([1, 2, 2, 1, 3]);
+
+console.log(max);
+
+function getMax(array) {
+    if (array.length === 0)
+        return undefined;
+    // let max = array[0];
+    // for (let i = 1; i < array.length; i++)
+    //     if (array[i] > max)
+    //         max = array[i];
+    // return max;
+
+    return array.reduce((a, b) => (a>b) ? a : b);
+    }
+
+//Movies
+
+const movies = [
+    {title: 'a', year: 2018, rating: 4.5},
+    {title: 'b', year: 2018, rating: 4.7},
+    {title: 'c', year: 2018, rating: 3},
+    {title: 'd', year: 2017, rating: 4.5},
+];
+
+//All movies in 2018 with rating > 4
+//Sort them by their rating
+//Descending order
+// Pick their title
+
+const titles = movies
+    .filter(m => m.year === 2018 && m.rating >=4)
+    .sort((a, b) => a.rating - b.rating)
+    .reverse()
+    .map(m => m.title)
+
+    console.log(titles);
